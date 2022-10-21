af51feb3a694b87f8dcb4cc9a9b07948aeecbc5a
kelenna favour <67713353+Kelekeys1234@users.noreply.github.com>
2022-10-21 05:19:44 -0700

    Update SurburbRepository.java

--- src/main/java/com/example/demo/repository/SurburbRepository.java
+++ src/main/java/com/example/demo/repository/SurburbRepository.java
@@ -1,14 +1,14 @@
 package com.example.demo.repository;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.SurburbModel;
 
 @Repository
-public interface SurburbRepository extends JpaRepository<SurburbModel, String> {
+public interface SurburbRepository extends JpaRepository<SurburbModel, UUID> {
 	public List<SurburbModel> findAllByPostCodeInOrderBySuburbName(List<String> postCode);
 }
